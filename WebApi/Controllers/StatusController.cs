2d26eaee46788bf54dfa4fbc20da4d95c30759c3
Renato Medeiros <renamedrj@gmail.com>
2024-08-24 09:14:14 -0300

    Adds Status endpoint

---
+++ WebApi/Controllers/StatusController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class StatusController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult HealthCheck()
+    {
+        return Ok("Ok");
+    }
+}
