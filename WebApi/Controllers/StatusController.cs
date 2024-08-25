f4c6cb7e2ee224057a808f0329378174a70ab1b7
Renato Medeiros <renamedrj@gmail.com>
2024-08-25 19:32:25 -0300

    v23

--- WebApi/Controllers/StatusController.cs
+++ WebApi/Controllers/StatusController.cs
@@ -6,13 +6,13 @@ namespace WebApi.Controllers;
 [ApiController]
 public class StatusController : ControllerBase
 {
-    private const int version = 22;
+    private const int version = 23;
 
     [HttpGet]
     public async Task<IActionResult> HealthCheck()
     {
         var init = DateTime.UtcNow;
-        await Task.Delay(TimeSpan.FromSeconds(3));
+        await Task.Delay(TimeSpan.FromSeconds(30));
         var fim = DateTime.UtcNow;
 
         return Ok($"{version} - {init:HH:mm:ss} - {fim:HH:mm:ss}");
--- img.ps1
+++ img.ps1
@@ -1,4 +1,4 @@
-$version=22
+$version=23
 Write-Host "version: $version"
 docker login
 docker build -t argotest:$version .
